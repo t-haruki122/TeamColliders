b11ff26ccbd070ed394f59c20c699f2df7612924
t-haruki122 m <231205122@ccmailg.meijo-u.ac.jp>
2025-05-21 18:46:01 +0900

    feat: DogEnemy

---
+++ Assets/Scripts/DogEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; // slider(HPバー)用
+
+public class DogEnemy : baseEnemy
+{
+    protected override void Spawn()
+    {
+        setBaseParams(
+            // 変更なし
+        );
+    }
+
+    protected override void Act()
+    {
+        
+    }
+}
